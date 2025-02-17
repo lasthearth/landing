3c48557bd05f1a1a6a5ed62f2129f6df8e3d3963
Ivan Burakov <lisovfox2002@gmail.com>
2025-02-17 16:54:24 +0300

    fix: Небольшая правка конфига

--- last-hearth-landing/vite.config.js
+++ last-hearth-landing/vite.config.js
@@ -4,9 +4,9 @@ export default defineConfig({
     build: {
         target: 'es2015',
         outDir: 'dist',
-        assetsDir: 'assets',
     },
     server: {
         port: 4200,
+        allowedHosts: ['lasthearth.ru'],
     },
 });
