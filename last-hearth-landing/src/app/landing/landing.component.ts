fa3aa4e5fc8573779abb7339daf96b218eac82e1
AlyonaBartul <tan-bokova@yandex.ru>
2025-03-13 23:19:01 +0300

    #7: Починены кнопки навигации и роутинг

--- last-hearth-landing/src/app/landing/landing.component.ts
+++ last-hearth-landing/src/app/landing/landing.component.ts
@@ -1,22 +1,82 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HeaderComponent } from '../layout/header/header.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ServerInformationService } from '../services/server-information.service';
-import { AsyncPipe, NgClass, NgTemplateOutlet } from '@angular/common';
-import { map } from 'rxjs';
-import { TuiIcon } from '@taiga-ui/core';
+import { Component, DestroyRef, inject } from "@angular/core";
+import { HeaderComponent } from "../layout/header/header.component";
+import {
+    ActivatedRoute,
+    NavigationEnd,
+    Router,
+    RouterLink,
+    RouterOutlet,
+} from "@angular/router";
+import { ServerInformationService } from "../services/server-information.service";
+import { AsyncPipe, NgClass, NgTemplateOutlet } from "@angular/common";
+import { filter, map } from "rxjs";
+import { TuiIcon } from "@taiga-ui/core";
+import { RouteKeys } from "../routes/enums/route-keys";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
     standalone: true,
-    selector: 'app-landing',
-    imports: [HeaderComponent, RouterOutlet, RouterLink, AsyncPipe, NgClass, TuiIcon, NgTemplateOutlet],
-    templateUrl: './landing.component.html',
-    styleUrl: './landing.component.less'
+    selector: "app-landing",
+    imports: [
+        HeaderComponent,
+        RouterOutlet,
+        RouterLink,
+        AsyncPipe,
+        NgClass,
+        TuiIcon,
+        NgTemplateOutlet,
+    ],
+    templateUrl: "./landing.component.html",
+    styleUrl: "./landing.component.less",
 })
 export class LandingComponent {
-    private readonly serverInformationService = inject(ServerInformationService);
+    private readonly serverInformationService = inject(
+        ServerInformationService
+    );
 
-    protected online$ = this.serverInformationService.getOnlinePlayersCount().pipe(map((info) => info.count));
+    private readonly activatedRoute = inject(ActivatedRoute);
 
-    protected select = 'home';
+    private readonly router = inject(Router);
+
+    private readonly destroyRef = inject(DestroyRef);
+
+    protected online$ = this.serverInformationService
+        .getOnlinePlayersCount()
+        .pipe(map((info) => info.count));
+
+    protected select = "home";
+
+    public constructor() {
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                takeUntilDestroyed(this.destroyRef)
+            )
+            .subscribe(() => {
+                let route = this.activatedRoute;
+
+                while (route.firstChild) {
+                    route = route.firstChild;
+                }
+
+                const routeKey = route.snapshot.data["route_keys"];
+
+                if (routeKey) {
+                    switch (routeKey) {
+                        case RouteKeys.home:
+                            this.select = "home";
+                            break;
+                        case RouteKeys.rules:
+                            this.select = "rules";
+                            break;
+                        case RouteKeys.market:
+                            this.select = "market";
+                            break;
+                        default:
+                            this.select = "home";
+                            break;
+                    }
+                }
+            });
+    }
 }
---
+++ last-hearth-landing/src/app/routes/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from "@angular/router";
+import { LandingComponent } from "../landing/landing.component";
+import { RulesComponent } from "../rules/rules.component";
+import { HomeComponent } from "../home/home.component";
+import { TitlesComponent } from "../titles/titles.component";
+import { MarketComponent } from "../market/market.component";
+import { RouteKeys } from "./enums/route-keys";
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: LandingComponent,
+        children: [
+            { path: "", redirectTo: "home", pathMatch: "full" },
+            {
+                path: "home",
+                component: HomeComponent,
+                data: { route_keys: RouteKeys.home },
+            },
+            {
+                path: "rules",
+                component: RulesComponent,
+                data: { route_keys: RouteKeys.rules },
+            },
+            {
+                path: "titles",
+                component: TitlesComponent,
+            },
+            {
+                path: "market",
+                component: MarketComponent,
+                data: { route_keys: RouteKeys.market },
+            },
+        ],
+    },
+];
