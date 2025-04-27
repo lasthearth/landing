17026d9de46016ee494bfa949e878bac1981828d
Ivan Burakov <lisovfox2002@gmail.com>
2025-04-27 15:50:37 +0300

    fix: редирект ссылки

--- last-hearth-landing/src/app/app.config.ts
+++ last-hearth-landing/src/app/app.config.ts
@@ -19,8 +19,8 @@ export const appConfig: ApplicationConfig = {
             config: buildAngularAuthConfig({
                 endpoint: 'https://logto.lasthearth.ru/',
                 appId: 'u9k3c8kap0lyhhs0o5jn1',
-                redirectUri: 'http://localhost:4200/home',
-                postLogoutRedirectUri: 'http://localhost:4200/home',
+                redirectUri: 'https://lasthearth.ru/home',
+                postLogoutRedirectUri: 'https://lasthearth.ru/home',
                 scopes: ['openid', 'profile', 'email', 'roles', 'question:create', 'user:verify'],
                 resource: 'https://api.lasthearth.ru',
             }),
