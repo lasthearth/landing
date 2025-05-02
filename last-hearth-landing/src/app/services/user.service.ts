052e2f70fc558ed8b073dca19d773259507f8387
Ivan Burakov <lisovfox2002@gmail.com>
2025-05-02 13:42:45 +0300

    fix: тест2

--- last-hearth-landing/src/app/services/user.service.ts
+++ last-hearth-landing/src/app/services/user.service.ts
@@ -3,7 +3,7 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { IUser } from './interface/i-user';
 import { jwtDecode } from "jwt-decode";
 import { IJwtTokenLh } from './interface/i-jwt-token-lh';
-import { BehaviorSubject, catchError, combineLatest, filter, first, of, tap } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, filter, first, of, switchMap, tap } from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
 
 @Injectable({
@@ -23,26 +23,78 @@ export class UserService {
     public accessToken!: string;
 
     constructor() {
-        this.oidcSecurityService.checkAuth().pipe(tap((authenticate) => {
-            if (authenticate) {
-                this.authState = authenticate.isAuthenticated;
-                this.userImage = authenticate.userData?.picture;
-                this.userName = authenticate.userData?.username;
-                combineLatest([
+        // this.oidcSecurityService.checkAuth().pipe(tap((authenticate) => {
+        //     if (authenticate) {
+        //         this.authState = authenticate.isAuthenticated;
+        //         this.userImage = authenticate.userData?.picture;
+        //         this.userName = authenticate.userData?.username;
+        //         combineLatest([
+        //             this.oidcSecurityService.getIdToken(),
+        //             this.oidcSecurityService.getAccessToken(),
+        //         ]).pipe(
+        //             tap(([idToken, accessToken]) => {
+        //                 this.accessToken = accessToken;
+        //                 const decoded = jwtDecode<IJwtTokenLh>(idToken);
+        //                 this.roles = decoded.roles ?? [];
+        //             }), catchError((e) => {
+        //                 console.error('Я упаль:', e);
+        //                 return of(null);
+        //             }), first()
+        //         ).subscribe();
+        //     }
+        // })).subscribe();
+
+        console.log('[Auth] Проверка авторизации началась');
+
+        this.oidcSecurityService.checkAuth().pipe(
+            tap(authResult => {
+                console.log('[Auth] Результат авторизации:', authResult);
+            }),
+            switchMap(authResult => {
+                if (!authResult.isAuthenticated) {
+                    console.warn('[Auth] Пользователь не аутентифицирован');
+                    return of(null);
+                }
+
+                this.authState = true;
+                this.userImage = authResult.userData?.picture;
+                this.userName = authResult.userData?.username;
+                console.log('[Auth] Пользователь аутентифицирован');
+                console.log('[Auth] Имя пользователя:', this.userName);
+                console.log('[Auth] Аватар пользователя:', this.userImage);
+
+                return combineLatest([
                     this.oidcSecurityService.getIdToken(),
                     this.oidcSecurityService.getAccessToken(),
                 ]).pipe(
+                    first(),
                     tap(([idToken, accessToken]) => {
+                        console.log('[Auth] Получены токены');
+                        console.log('[Auth] AccessToken:', accessToken);
+                        console.log('[Auth] IdToken:', idToken);
+
                         this.accessToken = accessToken;
-                        const decoded = jwtDecode<IJwtTokenLh>(idToken);
-                        this.roles = decoded.roles ?? [];
-                    }), catchError((e) => {
-                        console.error('Я упаль:', e);
+
+                        try {
+                            const decoded = jwtDecode<IJwtTokenLh>(idToken);
+                            this.roles = decoded.roles ?? [];
+                            console.log('[Auth] Роли пользователя:', this.roles);
+                        } catch (decodeError) {
+                            console.error('[Auth] Ошибка декодирования ID токена:', decodeError);
+                        }
+                    }),
+                    catchError(tokenError => {
+                        console.error('[Auth] Ошибка при получении токенов:', tokenError);
                         return of(null);
-                    }), first()
-                ).subscribe();
-            }
-        })).subscribe();
+                    })
+                );
+            }),
+            catchError(authError => {
+                console.error('[Auth] Ошибка при проверке авторизации:', authError);
+                return of(null);
+            }),
+            first()
+        ).subscribe();
     }
 
     public signIn(): void {
