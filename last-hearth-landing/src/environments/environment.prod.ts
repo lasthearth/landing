63345389bd1025587b7988126980e4f6a2c5d0ff
Ivan Burakov <lisovfox2002@gmail.com>
2025-12-18 01:20:12 +0300

    LH | fix: апдейт БИ

--- last-hearth-landing/src/environments/environment.prod.ts
+++ last-hearth-landing/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     redirectUri: 'https://lasthearth.ru/home',
     postLogoutRedirectUri: 'https://lasthearth.ru/home',
-    eastSuzerain: '136 на 16.12 14:08',
-    westSuzerain: '324 на 16.12 14:08',
+    eastSuzerain: '146 на 18.12 1:00',
+    westSuzerain: '354 на 18.12 1:00',
 };
--- last-hearth-landing/src/environments/environment.ts
+++ last-hearth-landing/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     redirectUri: 'http://localhost:4200/home',
     postLogoutRedirectUri: 'http://localhost:4200/home',
-    eastSuzerain: '136 на 16.12 14:08',
-    westSuzerain: '324 на 16.12 14:08',
+    eastSuzerain: '146 на 18.12 1:00',
+    westSuzerain: '354 на 18.12 1:00',
 };
