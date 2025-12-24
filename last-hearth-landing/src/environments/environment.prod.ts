521bb007044b2213811e877fd4897b34fc5f7a60
AlyonaBartul <tan-bokova@yandex.ru>
2025-12-24 22:09:52 +0300

    LH | fix: Апдейт БИ. feat: Добавлен п.6.13 правил

--- last-hearth-landing/src/environments/environment.prod.ts
+++ last-hearth-landing/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     redirectUri: 'https://lasthearth.ru/home',
     postLogoutRedirectUri: 'https://lasthearth.ru/home',
-    eastSuzerain: '539 на 23.12 10:33',
-    westSuzerain: '433 на 23.12 10:33',
+    eastSuzerain: '273 на 24.12 21:36',
+    westSuzerain: '573 на 24.12 21:36',
 };
