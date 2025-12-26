afca547778b747b290b494b9f9ef286afafb2e2d
AlyonaBartul <tan-bokova@yandex.ru>
2025-12-26 22:26:01 +0300

    LH | fix: Апдейт БИ.

--- last-hearth-landing/src/environments/environment.prod.ts
+++ last-hearth-landing/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     redirectUri: 'https://lasthearth.ru/home',
     postLogoutRedirectUri: 'https://lasthearth.ru/home',
-    eastSuzerain: '273 на 24.12 21:36',
-    westSuzerain: '573 на 24.12 21:36',
+    eastSuzerain: '371 на 26.12 22:26',
+    westSuzerain: '769 на 26.12 22:26',
 };
--- last-hearth-landing/src/environments/environment.ts
+++ last-hearth-landing/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     redirectUri: 'http://localhost:4200/home',
     postLogoutRedirectUri: 'http://localhost:4200/home',
-    eastSuzerain: '273 на 24.12 21:36',
-    westSuzerain: '573 на 24.12 21:36',
+    eastSuzerain: '371 на 26.12 22:26',
+    westSuzerain: '769 на 26.12 22:26',
 };
