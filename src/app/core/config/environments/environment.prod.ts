b318c074333accfa33f289f8943c51e9f8d36b99
Ivan <lisovfox2002@gmail.com>
2026-06-17 01:03:03 +0300

    LH | feat: фикс ошибки авторизации

--- src/app/core/config/environments/environment.prod.ts
+++ src/app/core/config/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    redirectUri: 'https://lasthearth.ru/home',
-    postLogoutRedirectUri: 'https://lasthearth.ru/home',
+    redirectUri: 'https://lasthearth.ru/home/',
+    postLogoutRedirectUri: 'https://lasthearth.ru/home/',
     apiUrl: 'https://api.lasthearth.ru/v1',
     logtoEndpoint: 'https://logto.lasthearth.ru',
     eastSuzerain: '2884 на 22.03 00:00',
